b1b86831e20fe7bc62d3e5c3958116fdd2990917
Edward <edwardgchancellor@gmail.com>
2020-04-21 19:10:31 +0200

    Start logic for trap collision

--- Assets/Scripts/MonoBehaviours/TrapInteraction.cs
+++ Assets/Scripts/MonoBehaviours/TrapInteraction.cs
@@ -1,8 +1,19 @@
 ﻿using UnityEngine;
+using System.Collections;
 
 public class TrapInteraction : MonoBehaviour 
 {
     public Trap trap;
+    private Animator anim;
+    private Animation currentAnim;
+    string animationState = "AnimationState";
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        currentAnim = GetComponent<Animation>();
+        anim.SetFloat("animspeed", 0f);
+    }
 
     void OnTriggerEnter2D(Collider2D collision)
     {
@@ -10,7 +21,23 @@ public class TrapInteraction : MonoBehaviour
         Debug.Log(gameObject.transform.position);
         pos.z = 0;
         gameObject.transform.position = pos;
-        gameObject.animator.controller.speed = 0;
+
+        //anim.SetInteger(animationState, 1);
+
+        StartCoroutine(MyCoroutine());
+
+        anim.SetFloat("animspeed", 1f);
+        Debug.Log(anim.speed);
+
+    }
+
+    IEnumerator MyCoroutine()
+    {
+        yield return new WaitForSeconds(2f);
+
+        anim.SetInteger(animationState, 1);
+       
+        yield return 0;
     }
 
 }
