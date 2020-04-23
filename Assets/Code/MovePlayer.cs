1d81c741cea68e74f0705e885f7ad9f559d9d810
Philipe Granhäll <13368707+Oxbrew@users.noreply.github.com>
2020-04-23 14:12:36 +0200

    Revert "the great merge 💪🏻"

--- Assets/Code/MovePlayer.cs
+++ Assets/Code/MovePlayer.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class MovePlayer : MonoBehaviour
 {
@@ -9,9 +8,6 @@ public class MovePlayer : MonoBehaviour
     public Rigidbody2D rb;
     public Vector3 posInit;
     public int i = 0;
-    public Collider cd;
-    public Text GameOver;
-    public Animator ani;
     
     // Start is called before the first frame update
     void Start()
@@ -23,72 +19,36 @@ public class MovePlayer : MonoBehaviour
     {
 	if(Input.GetKey(KeyCode.UpArrow) && i > 10){
 	    rb.MovePosition(rb.position + new Vector2(0,1));
-	    ani.SetFloat("up", 1f);
-	    //	    	    rb.velocity = new Vector3(0,1,0);
 	    i = 0;
 	}
 	if(Input.GetKey(KeyCode.DownArrow) && i > 10){
 	    rb.MovePosition(rb.position - new Vector2(0,1));
-	    // 	    	    rb.velocity = new Vector3(0,-1,0);
 	    i = 0;
-	    ani.SetFloat("down", 1f);
 	}
 	if(Input.GetKey(KeyCode.RightArrow) && i > 10){
 	    rb.MovePosition(rb.position + new Vector2(1,0));
-	    ani.SetFloat("right", 1f);
-	    //	    rb.velocity = new Vector3(1,0,0);
 	    i = 0;
 	}
 	if(Input.GetKey(KeyCode.LeftArrow) && i > 10){
 	    rb.MovePosition(rb.position - new Vector2(1,0));
-	    ani.SetFloat("left", 1f);
-	    //	    	    rb.velocity = new Vector3(-1,0,0);
 	    i = 0;
 	}
 	i++;
 	
 	if(Input.GetKeyUp(KeyCode.UpArrow)){
 	    i = 10;
-	    setAllAnimatorZero();
 	}
-	else if(Input.GetKeyUp(KeyCode.DownArrow)){
+	if(Input.GetKeyUp(KeyCode.DownArrow)){
 	    i = 10;
-	    setAllAnimatorZero();
 	}
-	else if(Input.GetKeyUp(KeyCode.RightArrow)){
+	if(Input.GetKeyUp(KeyCode.RightArrow)){
 	    i = 10;
-	    setAllAnimatorZero();
 	}
-	else if(Input.GetKeyUp(KeyCode.LeftArrow)){
+	if(Input.GetKeyUp(KeyCode.LeftArrow)){
 	    i = 10;
-	    setAllAnimatorZero();
-	}	    
-    }
+	}
 
-    void OnCollisionEnter2D(Collision2D col){	
-	if(col.gameObject.name == "Hole"){
-	    Debug.Log("OnCollisionEnter2D");
-
-	}
 	    
-    }
-
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-	if(other.gameObject.name == "Hole"){
-	    Debug.Log("OnCollisionEnter2D TRIGGER");
-	    FindObjectOfType<GameManager>().EndGame();
-
-	}
-
-    }
-
-    private void setAllAnimatorZero(){
-	ani.SetFloat("left", 0f);
-	ani.SetFloat("right", 0f);
-	ani.SetFloat("up", 0f);
-	ani.SetFloat("down", 0f);
-	ani.SetFloat("hole", 0f);
     }
 
 }
