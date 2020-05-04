cf360e5c490c8cf5c269417cbada6e0a1c8f3be8
darthvader211 <calincapitanu@gmail.com>
2020-05-04 15:26:54 +0200

    shit i was working on develop

--- Assets/Code/MultiMenu.cs
+++ Assets/Code/MultiMenu.cs
@@ -1,5 +1,13 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class MultiMenu : MonoBehaviour
 {
+    public void backFromLobby(){
+	SceneManager.LoadScene("Menu");
+    }
+
 }
--- Assets/Menu/MainMenu.cs
+++ Assets/Menu/MainMenu.cs
@@ -2,18 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+
 
 public class MainMenu : MonoBehaviour {
 
- public void PlayGame () 
- {
-     SceneManager.LoadScene("Level1_demo");
- }
- 
- public void QuitGame ()
- {
-  Debug.Log ("QUIT!");
-  Application.Quit();
- }
+    public string name;
+    
+    public void PlayGame () 
+    {
+	name = GameObject.Find("UsernameInput").GetComponent<TextMeshProUGUI>().text;
+	SceneManager.LoadScene("Level1_ch95");
+    }
+   
+	public void QuitGame ()
+    {
+	Debug.Log ("QUIT!");
+	Application.Quit();
+    }
   
 }
