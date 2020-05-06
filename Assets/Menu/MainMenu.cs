1047cfa00d0178b91bf58143b89e8be513c7d735
darthvader211 <calincapitanu@gmail.com>
2020-05-06 08:38:55 +0200

    lobby

--- Assets/Menu/MainMenu.cs
+++ Assets/Menu/MainMenu.cs
@@ -11,7 +11,7 @@ public class MainMenu : MonoBehaviour {
     
     public void PlayGame () 
     {
-	SceneManager.LoadScene("Level1_ch95");
+	SceneManager.LoadScene("Level1_calin");
     }
 
     public void setUsername(){
---
+++ Assets/Scripts/ServerScripts/Lobby.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Lobby : MonoBehaviour {
+
+    public static Lobby instance;
+    public string username;
+
+    private void Awake(){
+        if (instance == null){
+            instance = this;
+        }
+        else if (instance != this){
+            Debug.Log("Instance already exists, detroying object");
+            Destroy(this);
+        }
+	
+	Debug.Log(MainMenu.name);
+	username = MainMenu.name;
+//	username = "test";
+	StartCoroutine("connectToServer");
+    }
+
+    
+
+    public void startGame(){
+	GameManager.instance.inventoryCanvas.enabled = true;
+	GameObject.Find("Lobby").SetActive(false);
+    }
+
+    IEnumerator connectToServer(){
+	while(Client.instance == null){
+	    yield return null;
+	}
+	Client.instance.ConnectToServer();
+    }
+}
