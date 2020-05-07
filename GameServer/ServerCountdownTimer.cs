550bbaf73ed7ce806f03c45cf72a75f844982cba
dlwls1126 <dlwls1126@naver.com>
2020-05-07 08:29:06 +0200

    timer

--- GameServer/ServerCountdownTimer.cs
+++ GameServer/ServerCountdownTimer.cs
@@ -1,52 +1,59 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 
-public class ServerCountdownTimer
+namespace GameServer
 {
-    public ServerCountdownTimer instance;
-
-    public float currentTime = 0f;
-    public float startTime = 10f;
-    public bool isZero = false;
-
-    /*private void Awake()
+    public class ServerCountdownTimer
     {
-        if (instance == null)
+        public static ServerCountdownTimer instance;
+
+        public float currentTime = 0f;
+        public float startTime = 10f;
+        public bool isZero = false;
+
+        /*private void Awake()
         {
-            instance = this;
-        }
-        else if (instance != this)
+            if (instance == null)
+            {
+                instance = this;
+            }
+            else if (instance != this)
+            {
+                Destroy(this);
+            }
+        }*/
+        // Start is called before the first frame update
+        public void StartTimer()
         {
-            Destroy(this);
+
+            currentTime = startTime;
+            isZero = false;
         }
-    }*/
-    // Start is called before the first frame update
-    public void StartTimer()
-    {
 
-        currentTime = startTime;
-        isZero = false;
-    }
-
-    // Update is called once per frame
-    public void UpdateTimer()
-    {
-        currentTime -= 1; //* Time.deltaTime;
-        //GameObject.Find("Timer").GetComponent<TextMeshProUGUI>().text = currentTime.ToString("0");
-        //countdownText.text = currentTime.ToString("0");
-
-        if (currentTime <= 0)
+        // Update is called once per frame
+        public void UpdateTimer()
         {
-            currentTime = 0;
-            //GameObject.Find("Timer").GetComponent<TextMeshProUGUI>().text = "Time's up!";
-            isZero = true;
+            currentTime -= 1; //* Time.deltaTime;
+                              //GameObject.Find("Timer").GetComponent<TextMeshProUGUI>().text = currentTime.ToString("0");
+                              //countdownText.text = currentTime.ToString("0");
+
+            if (currentTime <= 0)
+            {
+                currentTime = 0;
+                //GameObject.Find("Timer").GetComponent<TextMeshProUGUI>().text = "Time's up!";
+                isZero = true;
+            }
+        }
+
+        public void Update()
+        {
+            UpdateTimer();
+            ServerSend.TimerInfo(instance);
+            Console.Write("sent currentTime");
         }
     }
-
-    public void Update()
-    {
-        UpdateTimer();
-    }
- }
+}
 
 
