a8a4068cb100477d667322fb92c59f5e4a18eb0e
dlwls1126 <dlwls1126@naver.com>
2020-05-13 09:05:32 +0200

    round works

--- Assets/Scripts/ServerScripts/CountdownTimer.cs
+++ Assets/Scripts/ServerScripts/CountdownTimer.cs
@@ -9,7 +9,7 @@ public class CountdownTimer : MonoBehaviour {
 
     public static CountdownTimer instance;
 
-    public float currentTime = 0f;
+    public float currentTime = 19f;
     public float startTime = 19f;
 
     private void Awake()
@@ -31,29 +31,31 @@ public class CountdownTimer : MonoBehaviour {
     // Update is called once per frame
     public void UpdateTimer()
     {
+        GameObject.Find("Timer").GetComponent<TextMeshProUGUI>().text = currentTime.ToString("0");
 
-        if (currentTime <= 19 && currentTime < 18)
+
+        if (currentTime <= 19 && currentTime > 18)
         {
             GameObject.Find("Timer").GetComponent<TextMeshProUGUI>().text = "3";
+            Debug.Log("3 countdown");
         }
-        else if (currentTime <= 18 && currentTime < 17)
+        else if (currentTime <= 18 && currentTime > 17)
         {
             GameObject.Find("Timer").GetComponent<TextMeshProUGUI>().text = "2";
         }
-        else if (currentTime <= 17 && currentTime < 16)
+        else if (currentTime <= 17 && currentTime > 16)
         {
             GameObject.Find("Timer").GetComponent<TextMeshProUGUI>().text = "1";
         }
-        else if (currentTime <= 16 && currentTime < 15)
+        else if (currentTime <= 16 && currentTime > 15)
         {
             GameObject.Find("Timer").GetComponent<TextMeshProUGUI>().text = "Round Start!";
         }
 
         //currentTime -= 1 * Time.deltaTime;
-        GameObject.Find("Timer").GetComponent<TextMeshProUGUI>().text = currentTime.ToString("0");
      //countdownText.text = currentTime.ToString("0");
 
-        if (currentTime <= 0){
+        else if (currentTime <= 0){
             currentTime = 0;
 	        GameObject.Find("Timer").GetComponent<TextMeshProUGUI>().text = "Time's up!";
         }
