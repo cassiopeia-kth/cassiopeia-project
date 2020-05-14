3e3f70ba628e77e9c92e12388b0041245ef61d91
Edward <edwardgchancellor@gmail.com>
2020-05-14 10:10:44 +0200

    credits fixed

---
+++ Assets/Menu/CreditsMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsMovement : MonoBehaviour
+{
+
+    public Vector3 StartPosition;
+    public Vector3 EndPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.transform.position = StartPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position != EndPosition)
+        {
+            
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, EndPosition, 120f * Time.deltaTime);
+        }
+    }
+}
