a56d658e1d8a3acdf7b6cf6dc228ac28ef867e10
Edward <edwardgchancellor@gmail.com>
2020-05-14 10:39:10 +0200

    credits reset fix

--- Assets/Menu/CreditsMovement.cs
+++ Assets/Menu/CreditsMovement.cs
@@ -7,6 +7,7 @@ public class CreditsMovement : MonoBehaviour
 
     public Vector3 StartPosition;
     public Vector3 EndPosition;
+    private bool startAgain = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,10 +17,20 @@ public class CreditsMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (startAgain)
+        {
+            gameObject.transform.position = StartPosition;
+            startAgain = false;
+        }
+        
         if (gameObject.transform.position != EndPosition)
         {
-            
             transform.localPosition = Vector3.MoveTowards(transform.localPosition, EndPosition, 120f * Time.deltaTime);
         }
+    }
+
+    public void ResetStartAgain()
+    {
+        startAgain = true;
     }
 }
